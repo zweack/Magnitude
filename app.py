10f8fc629f2820377cb8951d8eaa1c3a6c068eed
zweack <jeet88833@gmail.com>
2021-03-13 18:54:37 +0530

    updated procfile for heroku

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_hookserver import Hooks
 
 app = Flask(__name__)
 app.config['GITHUB_WEBHOOKS_KEY'] = os.environ.get('GITHUB_WEBHOOKS_KEY')
-app.config['VALIDATE_IP'] = (os.environ.get('GIT_HOOK_VALIDATE_IP', 'True').lower() not in ['false', '0'])
-app.config['VALIDATE_SIGNATURE'] = (os.environ.get('GIT_HOOK_VALIDATE_SIGNATURE', 'True').lower() not in ['false', '0'])
+app.config['VALIDATE_IP'] = False
+app.config['VALIDATE_SIGNATURE'] = False
 
 hooks = Hooks(app, url='/hooks')
 
